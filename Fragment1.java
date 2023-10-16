7572f460ed742379f6d5cea5dcb6b161f8c79c27
Elinaix <132971357+Elinaix@users.noreply.github.com>
2023-10-16 23:15:48 +0800

    Update Fragment1.java

--- Fragment1.java
+++ Fragment1.java
@@ -15,42 +15,33 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the  factory method to
- * create an instance of this fragment.
- */
 public class Fragment1 extends Fragment {
 
-    static class recycleviewactivity extends AppCompatActivity {
-
-        RecyclerView recyclerView;
+   RecyclerView recyclerView;
         List<String> list;
         Myadapater adapter;
         Context context;
 
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.tab1);
+            View view=inflater.inflate(R.layout.tab1, container, false);
 
-            recyclerView = findViewById(R.id.RecyclerView);
+            recyclerView = view.findViewById(R.id.RecyclerView);
+            context = view.getContext();
 
             list = new ArrayList<String>();
             for (int i = 0; i < 9; i++) {
                 list.add("这是第" + i + "行数据");
             }
+                adapter = new Myadapater(context, list);
+                recyclerView.setAdapter(adapter);
 
-            context = this;
-
-            adapter = new Myadapater(context, list);
-
-            LinearLayoutManager manager = new LinearLayoutManager(this);//布局
-            manager.setOrientation(RecyclerView.VERTICAL);
-            recyclerView.setLayoutManager(manager);
-            recyclerView.setAdapter(adapter);
-        }
+                LinearLayoutManager manager = new LinearLayoutManager(context);//布局
+                manager.setOrientation(RecyclerView.VERTICAL);
+                recyclerView.setLayoutManager(manager);
+        return view;
     }
 }
 
